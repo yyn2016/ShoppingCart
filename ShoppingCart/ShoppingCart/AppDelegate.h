8c6ae849d72a3f01f24c3558a4e4653f6c4850da
1 材知府 <czf1@caizhifudemac-mini.local>
2017-04-01 14:47:42 +0800

    初始化

---
+++ ShoppingCart/ShoppingCart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShoppingCart
+//
+//  Created by czf1 on 17/4/1.
+//  Copyright © 2017年 YYN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShoppingCart/ShoppingCart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShoppingCart
+//
+//  Created by czf1 on 17/4/1.
+//  Copyright © 2017年 YYN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
